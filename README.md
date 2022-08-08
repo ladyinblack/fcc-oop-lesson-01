9c7cfc2a395f5fb4ed7261afba0de5afc28345b0
ladyinblack <ladyinblack2010rs@gmail.com>
2022-08-08 09:15:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-cjyz3n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-cjyz3n)
---
+++ index.js
@@ -0,0 +1,20 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>Create a Basic JavaScript Object</h1>`;
+
+/** TODO:
+ * Create a dog object with name and numLegs properties, and set them to a string and a number, respectively.
+ * 
+ 
+ let dog = {
+ 
+ };
+ */
+
+let dog = {
+  name: 'Frodo',
+  numLegs: 4,
+};
